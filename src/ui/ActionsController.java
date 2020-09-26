2f41c8fbca35e7706ad287558b2725ae7a19a816
FernandaRojas152 <FernandaRojas152@hotmail.com>
2020-09-25 20:53:31 -0500

    All windows and controllers added, added pictures in resources thus it will be used in the interface

---
+++ src/ui/ActionsController.java
@@ -0,0 +1,35 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+
+public class ActionsController {
+
+    @FXML
+    private Label clientName;
+
+    @FXML
+    private RadioButton consignment;
+
+    @FXML
+    private RadioButton withdraw;
+
+    @FXML
+    private RadioButton cardPayment;
+
+    @FXML
+    private RadioButton cancellation;
+
+    @FXML
+    void makeAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void undoAction(ActionEvent event) {
+
+    }
+
+}
---
+++ src/ui/QueueController.java
@@ -0,0 +1,25 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class QueueController {
+
+    @FXML
+    private ListView<?> priorityQueue;
+
+    @FXML
+    private ListView<?> normalQueue;
+
+    @FXML
+    void attendClient(ActionEvent event) {
+
+    }
+
+    @FXML
+    void back(ActionEvent event) {
+
+    }
+
+}
