8160c836eb3fc88f121900136371875bf7aceff4
FernandaRojas152 <FernandaRojas152@hotmail.com>
2020-09-25 19:26:25 -0500

    Principal Window has now an icon, and a new design

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 
 /**
  * @version September 21th 2020
@@ -12,12 +13,20 @@ import javafx.scene.Scene;
  *
  */
 public class Main extends Application {
+	@SuppressWarnings("unused")
+	private PrincipalWindowController principal;
+	public Main() {
+		principal= new PrincipalWindowController();
+	}
 	@Override
 	public void start(Stage primaryStage) throws Exception{
 		try {
 			Parent root=FXMLLoader.load(getClass().getResource("PrincipalWindow.fxml"));
 			Scene scene= new Scene(root);
 			primaryStage.setScene(scene);
+			primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("bank-flat.png")));
+			primaryStage.setTitle("Bank S.A");
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
